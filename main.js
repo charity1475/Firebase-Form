3acf299841c5ace33624ebce573fb5d60ee33d21
Steinny <charitymbisi@gmail.com>
2020-09-20 22:22:47 +0300

    getting values

--- main.js
+++ main.js
@@ -2,5 +2,15 @@
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 function submitForm(e){
   e.preventDefault();
-  console.log(123);
+ /*getting values from field using id*/
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+}
+
+/*Function to get form values*/
+function getInputVal(id){
+  return document.getElementById(id).value;
 }

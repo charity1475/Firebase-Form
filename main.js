54fbbf38d8ada23de5fc9d58535d034d817bc554
Steinny <charitymbisi@gmail.com>
2020-09-20 22:16:20 +0300

    added functions

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 /*Listen for our form submit action */
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+function submitForm(e){
+  e.preventDefault();
+  console.log(123);
+}
